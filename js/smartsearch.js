f9645f7d2486b3cba15fb24c2f26e32c206a97da
Sean Madsen <sean@seanmadsen.com>
2017-08-05 23:01:19 -0600

    Improve accessibility of menu item

--- js/smartsearch.js
+++ js/smartsearch.js
@@ -9,8 +9,8 @@
   // Add Smart Search link to the main menu
   menu.find('li.crm-link-home')
     .after('<li class="menumain crm-SmartSearch">' +
-      '  <a class="crm-popup" title="Smart Search" href="/civicrm/smartsearch">' +
-      '    <i class="crm-i fa-search"></i>' +
+      '  <a class="crm-popup" title="Smart Search" aria-label="Smart Search" href="/civicrm/smartsearch">' +
+      '    <i class="crm-i fa-search" aria-hidden="true"></i>' +
       '  </a>' +
       '</li>');
 
