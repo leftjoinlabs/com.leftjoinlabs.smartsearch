b46f7e7b8714833094ed072dd47a4af59067f833
Sean Madsen <sean@seanmadsen.com>
2017-09-03 15:20:36 -0600

    Make standalone page function correctly

--- js/smartsearch.js
+++ js/smartsearch.js
@@ -1,6 +1,6 @@
 (function($, angular) {
 
-  // Add markup for the modal dialog to contain Smart Search
+  // Add markup for the modal dialog with the Smart Search app within
   $('body').append(
       '<div id="smartsearch-dialog" title="Smart Search">' +
       '  <div id="smartsearch-app">' +
@@ -8,32 +8,39 @@
       '  </div>' +
       '</div>');
 
+  var pageElement = document.getElementById('smartsearch-page');
+  var appElement = document.getElementById("smartsearch-app");
+
   // Bootstrap AngularJS for Smart Search
-  angular.bootstrap(document.getElementById("smartsearch-app"), ['smartsearch']);
+  angular.bootstrap(appElement, ['smartsearch']);
 
-  // Turn #ss-dialog into a jQuery UI modal dialog
-  $("#smartsearch-dialog").dialog({autoOpen: false});
-
+  // Menu stuff...
   var menu = $('#civicrm-menu');
-
   // Remove QuickSearch
-  // TODO only do this if user-defined preferences say so
   menu.find('#crm-qsearch').remove();
-
   // Add Smart Search link to the main menu
   menu.find('li.crm-link-home')
     .after('<li class="menumain" id="smartsearch-menu-link">' +
-      '  <a title="Smart Search" aria-label="Smart Search" >' +
+      '  <a title="Smart Search" aria-label="Smart Search" href="/civicrm/smartsearch">' +
       '    <i class="crm-i fa-search" aria-hidden="true"></i>' +
       '  </a>' +
       '</li>');
 
-  $('#smartsearch-menu-link a').click(function () {
-    $("#smartsearch-dialog").dialog("open");
-  });
+  if (pageElement) { // We are on the Smart Search standalone page...
+    // Move the app out from the modal and into the page
+    $(appElement).detach().appendTo(pageElement);
+  }
+  else { // We are on a normal CiviCRM page...
+    // Enable the jQuery UI modal dialog
+    $("#smartsearch-dialog").dialog({autoOpen: false});
+
+    // Make the menu link open the dialog
+    $('#smartsearch-menu-link a').click(function (e) {
+      e.preventDefault();
+      $("#smartsearch-dialog").dialog("open");
+      return false;
+    });
+  }
 
 })(CRM.$, angular);
-
-
-
 
